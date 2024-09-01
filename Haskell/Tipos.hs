b6769437640be3d1dfc6b669c66c0469d329959b
lucasqueiros <lucas.souza@ccc.ufcg.edu.br>
2024-09-01 16:55:49 -0300

    adicionando virgulas

--- Haskell/Tipos.hs
+++ Haskell/Tipos.hs
@@ -1,32 +1,32 @@
 module Tipos where
 
 data Cliente = Cliente {
-    cpf :: String
-    nome :: String
-    nivelCliente :: Int --varia entre 1 e 5, o default é 2
-    sexo :: String
-    idade :: Int
-    estadoCivil :: String
-    nivelDeRisco :: Float 
-    telefone :: String
-    listaSeguros :: [Seguro] -- é pra ser uma lista, nao sei como implementar ainda
-    statusFinanceiro :: Bool --true se tiver em dia com o pagamento
-    numSinistros :: Int
+    cpf :: String,
+    nome :: String,
+    nivelCliente :: Int, --varia entre 1 e 5, o default é 2
+    sexo :: String,
+    idade :: Int,
+    estadoCivil :: String,
+    nivelDeRisco :: Float,
+    telefone :: String,
+    listaSeguros :: [Seguro], -- é pra ser uma lista, nao sei como implementar ainda
+    statusFinanceiro :: Bool, --true se tiver em dia com o pagamento
+    numSinistros :: Int,
     tipoAutomovel :: String
 } deriving (Show, Eq)
 
 data Seguro = Seguro {
-    idSeguro :: String
-    cpfCliente :: String
-    automovel :: String
-    tipoContrato :: String --basico, tradicional ou premium
+    idSeguro :: String,
+    cpfCliente :: String,
+    automovel :: String,
+    tipoContrato :: String, --basico, tradicional ou premium
 } deriving (Show, Eq)
 
 data Sinistro = Sinistro {
-    idSinistro :: String
-    cpfCliente :: String
-    idSeguro :: String
-    nivelAcidente :: String --leve, médio ou perca total
-    custo :: Float -- vai ser calculado com base em variaveis
-    data :: String
+    idSinistro :: String,
+    cpfCliente :: String,
+    idSeguro :: String,
+    nivelAcidente :: String, --leve, médio ou perca total
+    custo :: Float, -- vai ser calculado com base em variaveis
+    data :: String,
 } deriving (Show, Eq)
